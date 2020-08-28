0d858e9e201154609b1d985866d41023b93208d8
Nguyen Huu Hoa <huuhoa@gmail.com>
2020-08-28 17:39:05 +0700

    cleanup ads on fsblog

--- etr_fsblog.py
+++ etr_fsblog.py
@@ -5,3 +5,9 @@ class FsblogExtractor(Extractor):
     def get_title(self):
         h1 = self.bs.find('h1', class_='entry-title')
         return h1.text
+
+    def cleanup(self):
+        self.remove_tag(self.site, 'p', attrs={
+            'style': 'color: black; background: #ffffcc none repeat scroll 0% 0%; text-align: center;'
+        })
+        super(FsblogExtractor, self).cleanup()
