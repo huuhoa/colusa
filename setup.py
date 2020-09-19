6077d80e1e1113a780a1a81fec839bb9af70f52f
Nguyen Huu Hoa <huuhoa@gmail.com>
2020-09-19 08:57:40 +0700

    chg: add setup.py for easier installation

---
+++ setup.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='symphony',
+      version='0.1.0',
+      description='Render website to ebook to make it easier to read on devices',
+      url='http://github.com/huuhoa/symphony',
+      author='Huu Hoa NGUYEN',
+      author_email='huuhoa@gmail.com',
+      license='MIT',
+      packages=['symphony'],
+      install_requires=[
+          'beautifulsoup4==4.9.1',
+          'certifi==2020.6.20',
+          'chardet==3.0.4',
+          'idna==2.10',
+          'requests==2.24.0',
+          'soupsieve==2.0.1',
+          'urllib3==1.25.10',
+          'python-dateutil~=2.8.1'
+      ],
+      entry_points={
+          'console_scripts': [
+              'symphony = symphony.cli:main'
+          ]
+      })
---
+++ symphony/cli.py
@@ -0,0 +1,23 @@
+import argparse
+
+from symphony import Symphony, read_configuration_file
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--new', '-n', type=bool, default=False,
+                        help='Generate new configuration file. '
+                             'File name will be specified in the --input parameter')
+    parser.add_argument('--input', '-i', type=str, help='Configuration file')
+    args = parser.parse_args()
+    if args.new:
+        Symphony.generate_new_configuration(args.input)
+        exit(0)
+
+    configs = read_configuration_file(args.input)
+    symp = Symphony(configs)
+    symp.main()
+
+
+if __name__ == '__main__':
+    main()
