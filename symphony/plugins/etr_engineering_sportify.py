878850b1716a43b10c546fd0bd64377d811b6e2f
Nguyen Huu Hoa <huuhoa@gmail.com>
2020-09-20 21:50:58 +0700

    chg: add support for knowledgegraph.today

--- symphony/plugins/etr_engineering_sportify.py
+++ symphony/plugins/etr_engineering_sportify.py
@@ -3,10 +3,6 @@ from symphony.etr import Extractor, register_extractor
 
 @register_extractor('engineering.atspotify.com')
 class EngineeringSpotifyExtractor(Extractor):
-    def internal_init(self):
-        self.site = self.bs.find('main', attrs={'id': 'main'})
-
     def cleanup(self):
         self.remove_tag(self.site, 'div', attrs={'class': 'share-block'})
-        self.remove_tag(self.site, 'aside', attrs={'class': 'asidebar'})
         super(EngineeringSpotifyExtractor, self).cleanup()
