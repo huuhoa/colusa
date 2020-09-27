ca678929d0d36352c9eed19292823d2021c44a4f
Nguyen Huu Hoa <huuhoa@gmail.com>
2020-09-27 15:57:09 +0700

    chg: add help str to make error report more concise

--- colusa/cli.py
+++ colusa/cli.py
@@ -14,19 +14,27 @@ def parse_args():
     commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    init_parser = commands.add_parser('init')
+    init_parser = commands.add_parser('init',
+                                      help='Generate configuration file with basic structure which '
+                                           'contains every input fields required by colusa')
     init_parser.set_defaults(func=init)
-    init_parser.add_argument('output', type=str, help='Configuration file')
+    init_parser.add_argument('output', type=str, help='Configuration file. '
+                                                      'File extension should be either json or yml')
 
-    generate_parser = commands.add_parser('generate')
+    generate_parser = commands.add_parser('generate',
+                                          help='Generate ebook source data based on input configuration')
     generate_parser.set_defaults(func=generate)
-    generate_parser.add_argument('input', type=str, help='Configuration file')
+    generate_parser.add_argument('input', type=str, help='Configuration file. '
+                                                         'File extension should be either json or yml')
 
     return parser.parse_args()
 
 
 def init(args):
-    Colusa.generate_new_configuration(args.output)
+    try:
+        Colusa.generate_new_configuration(args.output)
+    except ConfigurationError as e:
+        logs.error(e)
 
 
 def generate(args):
