ef8b4b248666e0dcb75ae27ad80c631c130f90b1
Nguyen Huu Hoa <huuhoa@gmail.com>
2021-05-10 18:14:46 +0700

    chg: support for parsing hbr.org

---
+++ colusa/plugins/etr_hbr.py
@@ -0,0 +1,18 @@
+import re
+
+from bs4 import Tag
+
+from colusa.etr import Extractor, Transformer, register_extractor, register_transformer
+
+
+@register_extractor('//hbr.org')
+class HBRExtractor(Extractor):
+    def internal_init(self):
+        self.site = self.bs.find('div', class_='article-body standard-content')
+
+    def cleanup(self):
+        self.remove_tag(self.site, 'div', attrs={'class': 'left-rail--container'})
+        self.remove_tag(self.site, 'div', attrs={'class': 'translate-message'})
+        self.remove_tag(self.site, 'div', attrs={'class': 'right-rail--container'})
+        self.remove_tag(self.site, 'div', attrs={'class': 'post-container'})
+
