c853a01a3083d81c8b39a0059e871c79eb4dba2e
Nguyen Huu Hoa <huuhoa@gmail.com>
2021-08-28 12:39:52 +0700

    chg: update unit test to mock download_content

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -10,14 +10,19 @@ import os.path
 
 def download_image_mocked(url_path, *args, **kwargs):
     import urllib
-    print(url_path)
-    print(args)
-    print(kwargs)
     print(f'call download_image with {url_path}')
     result = urllib.parse.urlsplit(url_path)
     p = pathlib.PurePath(result.path)
     image_name = f'{get_hexdigest(url_path)}{p.suffix}'
     return image_name
+
+
+def download_content_mocked(url_path: str):
+    print(f'call colusa.Colusa.download_content with path {url_path}')
+    cached_file_path = os.path.join('tests-cached', f'{get_hexdigest(url_path)}.html')
+    with open(cached_file_path, 'rt', encoding='utf-8') as file_in:
+        content = file_in.read()
+    return content
 
 
 class ColusaIntegrationTestCase(unittest.TestCase):
@@ -33,8 +38,36 @@ class ColusaIntegrationTestCase(unittest.TestCase):
         self.assertTrue(os.path.exists('tests-dist/images/abc.png'))
         self.assertFalse(os.path.exists('tests-dist/.cached/images/abc.png'))
 
+    @patch('colusa.Colusa.download_content')
+    def test_mock_download_content(self, mock_download_content):
+        def side_effect(url_path: str):
+            print(f'call colusa.Colusa.download_content with path {url_path}')
+            cached_file_path = os.path.join('tests-cached', f'{get_hexdigest(url_path)}.html')
+            with open(cached_file_path, 'rt', encoding='utf-8') as file_in:
+                content = file_in.read()
+            return content
+
+        mock_download_content.side_effect = side_effect
+
+        configs = {
+            "title": "test: test case",
+            "author": "tester",
+            "version": "v1.0",
+            "homepage": "dummy",
+            "output_dir": "tests-dist",
+            "multi_part": False,
+            "parts": [],
+            "urls": [
+                "https://staffeng.com/guides/overview-overview",
+            ]
+        }
+        runner = Colusa(configs)
+        runner.generate()
+        mock_download_content.assert_called_once_with("https://staffeng.com/guides/overview-overview")
+
+    @patch('colusa.Colusa.download_content')
     @patch('colusa.utils.download_image')
-    def test_whole_program(self, mock_download_image):
+    def test_whole_program(self, mock_download_image, mock_download_content):
         urls = [
             "https://staffeng.com/guides/overview-overview",
             "https://tech.trivago.com/2016/03/24/team-work-made-simple-with-guilds/",
@@ -48,6 +81,7 @@ class ColusaIntegrationTestCase(unittest.TestCase):
         Mocking os.path.exists with using side_effect
         """
         mock_download_image.side_effect = download_image_mocked
+        mock_download_content.side_effect = download_content_mocked
 
         for url in urls:
             configs = {
