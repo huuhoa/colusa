027aa77151f43286b5cf540138fd5989af2988ad
Huu Hoa NGUYEN <huuhoa@gmail.com>
2025-12-07 18:37:52 +0700

    Bump version: 0.13.0 → 0.14.0

--- src/colusa/_version.py
+++ src/colusa/_version.py
@@ -1,6 +1,6 @@
 __author__ = """Huu Hoa NGUYEN"""
 __email__ = "huuhoa@gmail.com"
-__version__ = '0.13.0'
+__version__ = '0.14.0'
 __copyright__ = "Copyright (c) 2020-2021 Huu Hoa NGUYEN"
 __license__ = "MIT License"
 __description__ = "Render website to ebook to make it easier to read on devices"
